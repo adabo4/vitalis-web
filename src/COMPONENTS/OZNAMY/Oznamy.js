455d91dba1aa8f9283df575d427204f9f6f559dd
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-08-13 22:55:39 +0200

    added slide-in effect on Oznamy

--- src/COMPONENTS/OZNAMY/Oznamy.js
+++ src/COMPONENTS/OZNAMY/Oznamy.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./oznamy.css";
+import useScrollIn from '../helpers/useScrollIn';
 
 export default function Oznamy() {
+    const addToRefs = useScrollIn()
     return (
 
         <>
@@ -10,7 +12,7 @@ export default function Oznamy() {
                     <div className="oznamy-header">
                         <h1>OZNAMY</h1>
                     </div>
-                    <p>Dňa 22.7. 2024 od 10:00 - 16:00 hod. bude v Dermocentre Vitalis prebiehať PROMO AKCIA na kozmetiku Avène, A-Derma, Ducray a Klorane,
+                    <p ref={addToRefs} className='slide-in'>Dňa 22.7. 2024 od 10:00 - 16:00 hod. bude v Dermocentre Vitalis prebiehať PROMO AKCIA na kozmetiku Avène, A-Derma, Ducray a Klorane,
                         spojená s diagnostikou pleti. Samozrejmosťou budú aj zľavy na každý jeden produkt.
                     </p>
 
