ea86f3f81a12ecbc39e5204ca2dda57d44d07d5b
adabo4 <130857407+adabo4@users.noreply.github.com>
2025-03-04 20:34:03 +0100

    leaflet update

--- src/COMPONENTS/DISCOUNTS/discountsimg.js
+++ src/COMPONENTS/DISCOUNTS/discountsimg.js
@@ -5,29 +5,30 @@ const discountsimg = [
         alt: 'Avene'
     },
     {
-        url: require("../img/DISCOUNTS/lrp.jpg"),
-        src: require("../img/DISCOUNTS/lrp.jpg"),
-        alt: 'La Roche Posay'
+        url: require("../img/DISCOUNTS/ducray.jpg"),
+        src: require("../img/DISCOUNTS/ducray.jpg"),
+        alt: 'Ducray'
     },
+    {
+        url: require("../img/DISCOUNTS/avene2.jpg"),
+        src: require("../img/DISCOUNTS/avene2.jpg"),
+        alt: 'Avene'
+    },
+
     {
         url: require("../img/DISCOUNTS/bioderma.png"),
         src: require("../img/DISCOUNTS/bioderma.png"),
         alt: 'Bioderma'
     },
     {
-        url: require("../img/DISCOUNTS/lrp2.jpg"),
-        src: require("../img/DISCOUNTS/lrp2.jpg"),
-        alt: 'La Roche Posay'
+        url: require("../img/DISCOUNTS/klorane.jpg"),
+        src: require("../img/DISCOUNTS/klorane.jpg"),
+        alt: 'Klorane'
     },
     {
-        url: require("../img/DISCOUNTS/cerave.jpg"),
-        src: require("../img/DISCOUNTS/cerave.jpg"),
-        alt: 'Cerave'
-    },
-    {
-        url: require("../img/DISCOUNTS/vichy.jpg"),
-        src: require("../img/DISCOUNTS/vichy.jpg"),
-        alt: 'Vichy'
+        url: require("../img/DISCOUNTS/aderma.jpg"),
+        src: require("../img/DISCOUNTS/aderma.jpg"),
+        alt: 'Aderma'
     }
 ]
 
