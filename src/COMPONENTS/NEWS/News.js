2fe71fe03456a95c457781cbc6db2f8778259f22
adabo4 <130857407+adabo4@users.noreply.github.com>
2025-06-03 19:38:06 +0200

    news update

--- src/COMPONENTS/NEWS/News.js
+++ src/COMPONENTS/NEWS/News.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import "./news.css";
 import useScrollIn from '../helpers/useScrollIn';
-import tereziaimg from "../img/terezia.jpg"
+
+
 
 export default function News() {
     const addToRefs = useScrollIn()
@@ -12,17 +13,21 @@ export default function News() {
                     <div className="news-header">
                         <h1>AKTUALITY</h1>
                     </div>
-                    <p ref={addToRefs} className='new slide-in'>Dňa 12.05. 2025 od 10:00 - 16:00 hod. bude v Dermocentre Vitalis prebiehať PROMO AKCIA na kozmetiku Avène, A-Derma, Ducray a Klorane,
+
+                    <p ref={addToRefs} className='new slide-in'>Dňa 17.06.2025 od 10:00 - 16:00 hod. bude v Dermocentre Vitalis prebiehať PROMO AKCIA na kozmetiku Avène, A-Derma, Ducray a Klorane,
                         spojená s diagnostikou pleti. Samozrejmosťou budú aj zľavy na každý jeden produkt.
                     </p>
-                    <div className='terezia'>
+
+
+
+                    {/* <div className='terezia'>
                         <p ref={addToRefs} className='slide-in'>Príďte na špeciálnu promo akciu TEREZIA COMPANY - 20.05.2025 -
                             kde objavíte prírodné produkty pre Vaše zdravie a krásu!
                             Získajte odborné poradenstvo a výhodné zľavy na produkty.
                             Terezia je český výrobca prírodných doplnkov stravy zo zdraviu prospešných húb a rastlín bez konzervačných látok.
                         </p>
                         <img src={tereziaimg} alt="Terezia Company" />
-                    </div>
+                    </div> */}
                 </div>
 
             </div>
