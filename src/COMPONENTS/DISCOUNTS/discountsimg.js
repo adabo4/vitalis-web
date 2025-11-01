5b50c2b9e6b1d5332be2b96c8f3f6ce0685c254a
adabo4 <adabo4@gmail.com>
2025-11-01 13:42:07 +0100

    november leaflet and dicsounts update

--- src/COMPONENTS/DISCOUNTS/discountsimg.js
+++ src/COMPONENTS/DISCOUNTS/discountsimg.js
@@ -1,31 +1,34 @@
 const discountsimg = [
     {
-        url: require("../img/DISCOUNTS/aderma.jpg"),
-        src: require("../img/DISCOUNTS/aderma.jpg"),
+        url: require("../img/DISCOUNTS/aderma.webp"),
+        src: require("../img/DISCOUNTS/aderma.webp"),
         alt: 'Akcia na kozmetiku Aderma – Trnava, Lekáreň Vitalis"'
     },
     {
-        url: require("../img/DISCOUNTS/avene.jpg"),
-        src: require("../img/DISCOUNTS/avene.jpg"),
+        url: require("../img/DISCOUNTS/klorane.webp"),
+        src: require("../img/DISCOUNTS/klorane.webp"),
+        alt: 'Akcia na kozmetiku Ducray – Trnava, Lekáreň Vitalis"'
+    },
+    {
+        url: require("../img/DISCOUNTS/avene.webp"),
+        src: require("../img/DISCOUNTS/avene.webp"),
         alt: 'Akcia na kozmetiku Avène – Trnava, Lekáreň Vitalis"'
     },
-
     {
-        url: require("../img/DISCOUNTS/bioderma.png"),
-        src: require("../img/DISCOUNTS/bioderma.png"),
+        url: require("../img/DISCOUNTS/bioderma.webp"),
+        src: require("../img/DISCOUNTS/bioderma.webp"),
         alt: 'Akcia na kozmetiku Bioderma – Trnava, Lekáreň Vitalis"'
     },
     {
-        url: require("../img/DISCOUNTS/ducray.jpg"),
-        src: require("../img/DISCOUNTS/ducray.jpg"),
-        alt: 'Akcia na kozmetiku Ducray – Trnava, Lekáreň Vitalis"'
+        url: require("../img/DISCOUNTS/bioderma2.webp"),
+        src: require("../img/DISCOUNTS/bioderma2.webp"),
+        alt: 'Akcia na kozmetiku Bioderma – Trnava, Lekáreň Vitalis"'
     },
     {
-        url: require("../img/DISCOUNTS/klorane.jpg"),
-        src: require("../img/DISCOUNTS/klorane.jpg"),
+        url: require("../img/DISCOUNTS/ducray.webp"),
+        src: require("../img/DISCOUNTS/ducray.webp"),
         alt: 'Akcia na kozmetiku Ducray – Trnava, Lekáreň Vitalis"'
     }
-
 ]
 
 export default discountsimg;
