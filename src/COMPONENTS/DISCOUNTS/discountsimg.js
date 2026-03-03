f0064b6268d9baf602a706232dfa6e64ca1dbb57
adabo4 <adabo4@gmail.com>
2026-03-03 15:01:04 +0100

    updated discounts

--- src/COMPONENTS/DISCOUNTS/discountsimg.js
+++ src/COMPONENTS/DISCOUNTS/discountsimg.js
@@ -2,14 +2,14 @@
 
 const discountsimg = [
     {
-        url: require("../img/DISCOUNTS/lrp.webp"),
-        src: require("../img/DISCOUNTS/lrp.webp"),
-        alt: 'Akcia na kozmetiku La Roche-Posay – Trnava, Lekáreň Vitalis"'
+        url: require("../img/DISCOUNTS/aderma.webp"),
+        src: require("../img/DISCOUNTS/aderma.webp"),
+        alt: 'Akcia na kozmetiku Aderma – Trnava, Lekáreň Vitalis"'
     },
     {
-        url: require("../img/DISCOUNTS/vichy.webp"),
-        src: require("../img/DISCOUNTS/vichy.webp"),
-        alt: 'Akcia na kozmetiku Vichy – Trnava, Lekáreň Vitalis"'
+        url: require("../img/DISCOUNTS/ducray.webp"),
+        src: require("../img/DISCOUNTS/ducray.webp"),
+        alt: 'Akcia na kozmetiku Ducray – Trnava, Lekáreň Vitalis"'
     },
     {
         url: require("../img/DISCOUNTS/avene.webp"),
@@ -17,14 +17,9 @@ const discountsimg = [
         alt: 'Akcia na kozmetiku Avène – Trnava, Lekáreň Vitalis"'
     },
     {
-        url: require("../img/DISCOUNTS/bioderma.webp"),
-        src: require("../img/DISCOUNTS/bioderma.webp"),
-        alt: 'Akcia na kozmetiku Bioderma – Trnava, Lekáreň Vitalis"'
-    },
-    {
-        url: require("../img/DISCOUNTS/cerave.webp"),
-        src: require("../img/DISCOUNTS/cerave.webp"),
-        alt: 'Akcia na kozmetiku Cerave – Trnava, Lekáreň Vitalis"'
+        url: require("../img/DISCOUNTS/klorane.webp"),
+        src: require("../img/DISCOUNTS/klorane.webp"),
+        alt: 'Akcia na kozmetiku Klorane – Trnava, Lekáreň Vitalis"'
     }
 ]
 
