cd3e0f257726aa46cb8ca1306aeac21339d6aa2b
adabo4 <adabo4@gmail.com>
2026-04-03 14:13:44 +0200

    updated leaflet and discounts

--- src/COMPONENTS/COUPONS/Coupons.js
+++ src/COMPONENTS/COUPONS/Coupons.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Coupons() {
     const coupons = [
         {
-            src: "../coupons/poukazka1.jpg"
+            src: "../coupons/poukazka1.webp"
         },
         {
             src: "../coupons/poukazka2.png"
--- src/COMPONENTS/DISCOUNTS/discountsimg.js
+++ src/COMPONENTS/DISCOUNTS/discountsimg.js
@@ -7,20 +7,21 @@ const discountsimg = [
         alt: 'Akcia na kozmetiku Aderma – Trnava, Lekáreň Vitalis"'
     },
     {
-        url: require("../img/DISCOUNTS/ducray.webp"),
-        src: require("../img/DISCOUNTS/ducray.webp"),
-        alt: 'Akcia na kozmetiku Ducray – Trnava, Lekáreň Vitalis"'
-    },
-    {
         url: require("../img/DISCOUNTS/avene.webp"),
         src: require("../img/DISCOUNTS/avene.webp"),
         alt: 'Akcia na kozmetiku Avène – Trnava, Lekáreň Vitalis"'
     },
     {
-        url: require("../img/DISCOUNTS/klorane.webp"),
-        src: require("../img/DISCOUNTS/klorane.webp"),
-        alt: 'Akcia na kozmetiku Klorane – Trnava, Lekáreň Vitalis"'
-    }
+        url: require("../img/DISCOUNTS/aderma2.webp"),
+        src: require("../img/DISCOUNTS/aderma2.webp"),
+        alt: 'Akcia na kozmetiku Aderma – Trnava, Lekáreň Vitalis"'
+    },
+    {
+        url: require("../img/DISCOUNTS/avene2.webp"),
+        src: require("../img/DISCOUNTS/avene2.webp"),
+        alt: 'Akcia na kozmetiku Avène – Trnava, Lekáreň Vitalis"'
+    },
+
 ]
 
 export default discountsimg;
